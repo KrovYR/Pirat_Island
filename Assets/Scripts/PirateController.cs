2db0771836bd4d87fad3896831f551ce225a7f26
AlexeyAndrianov <52441522+KrovYR@users.noreply.github.com>
2019-07-03 14:17:59 +0300

    Initial commit

---
+++ Assets/Scripts/PirateController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        Vector2 position = transform.position;
+
+        //Move
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
+
+        transform.position = position;
+
+        mouseX = position.x - mouseX;
+        mouseY = position.y - mouseY;
+
+
+    }
+}
